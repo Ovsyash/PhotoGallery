0a556b623bd6e96e977570c51a84ed6bb97889ca
Ovsyash <r1sk-57@mail.ru>
2022-08-28 12:50:12 +0300

    + Глава 26

--- app/src/main/java/com/ovsyannikov/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/ovsyannikov/photogallery/PhotoGalleryFragment.kt
@@ -6,11 +6,10 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
@@ -35,6 +34,7 @@ class PhotoGalleryFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         retainInstance = true
+        setHasOptionsMenu(true)
 
         photoGalleryViewModel = ViewModelProviders.of(this)[PhotoGalleryViewModel::class.java]
 
@@ -49,8 +49,8 @@ class PhotoGalleryFragment : Fragment() {
 
     private class PhotoHolder(itemImageView: ImageView)
         : RecyclerView.ViewHolder(itemImageView) {
-            val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
-        }
+        val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
+    }
 
     private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>)
         : RecyclerView.Adapter<PhotoHolder>() {
@@ -64,7 +64,7 @@ class PhotoGalleryFragment : Fragment() {
                 parent,
                 false
             ) as ImageView
-                return PhotoHolder(view)
+            return PhotoHolder(view)
         }
 
         override fun onBindViewHolder(
@@ -122,6 +122,44 @@ class PhotoGalleryFragment : Fragment() {
         lifecycle.removeObserver(
             thumbnailDownloader.fragmentLifecycleObserver
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_photo_gallery, menu)
+
+        val searchItem: MenuItem = menu.findItem(R.id.menu_item_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.apply {
+
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(queryText: String): Boolean {
+                    Log.d(TAG, "QueryTextSubmit: $queryText")
+                    photoGalleryViewModel.fetchPhotos(queryText)
+                    return true
+                }
+
+                override fun onQueryTextChange(queryText: String): Boolean {
+                    Log.d(TAG, "QueryTextChange: $queryText")
+                    return false
+                }
+            })
+
+            setOnSearchClickListener {
+                searchView.setQuery(photoGalleryViewModel.searchTerm, false)
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item_clear -> {
+                photoGalleryViewModel.fetchPhotos("")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
